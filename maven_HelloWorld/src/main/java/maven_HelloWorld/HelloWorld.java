110e0ff4375856ab3261ceeb66b743680861407a
Pallavi-BA <pallavibapallavi@gmail.com>
2024-07-30 20:04:27 +0530

    Hello World

---
+++ maven_HelloWorld/src/main/java/maven_HelloWorld/HelloWorld.java
@@ -0,0 +1,15 @@
+package maven_HelloWorld;
+
+public class HelloWorld 
+{
+	public void display() 
+	{
+		System.out.println ("Hello World");
+	}
+	
+	public static void main (String [ ] args)
+	{
+		HelloWorld obj=new HelloWorld();
+		obj.display();
+	}
+}
